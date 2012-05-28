3b6a08e536f9791034235e173da695ce7d723a23
Daniel Kaufman <daniel.garrett.kaufman@gmail.com>
2012-05-28 10:08:01 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VpsDummyApp::Application.config.secret_token = 'b36f33ce46707b1ff9aa4ae30d16ceb4f45ec519b6b0560130483a03daaddd419192ece89a2cef41ec45897acc0c901216ccfa145a1f408d1d519537a0ed7dd5'
